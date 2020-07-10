493731cefa3401d5ac39ff5907b700560f716e01
harluss <harluss@gmail.com>
2020-07-11 00:13:00 +0100

    fixed broken pr workflow test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 it('renders raining cats text', () => {
 	const { getByText } = render(<App />);
-	const text = getByText(/Raining rats/i);
+	const text = getByText(/Raining cats/i);
 	expect(text).toBeInTheDocument();
 });
