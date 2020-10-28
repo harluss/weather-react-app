6ba4b7a5985ebb6f9ae26f235f1b252ed1539797
harluss <26788867+harluss@users.noreply.github.com>
2020-10-28 12:03:07 +0000

    added axios, bootstrap, open weather api key and created user input form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import SearchForm from 'components/SearchForm';
 
 const App = () => {
-	return <p>Raining cats and dogs...</p>;
+	return (
+		<Container className='my-4'>
+			<SearchForm />
+			<p>Raining cats and dogs...</p>
+		</Container>
+	);
 };
 
 export default App;
