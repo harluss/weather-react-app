bbe3c044addd344adfe54868c7f722c94d5b45fc
harluss <26788867+harluss@users.noreply.github.com>
2021-03-19 10:15:31 +0000

    added tests for Input

--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -1,3 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import Input from 'components/Input';
+
+const setTestProps = () => ({
+  label: 'city',
+  name: 'city',
+  touched: {},
+  errors: {},
+});
+
 describe('Input component', () => {
-  it.todo('add tests');
+  let testProps;
+  beforeAll(() => {
+    testProps = setTestProps();
+  });
+
+  it('displays correct label', async () => {
+    render(<Input {...testProps} />);
+
+    const label = await screen.findByText(testProps.label);
+    expect(label).toBeInTheDocument();
+  });
+
+  it('has a default type of text', async () => {
+    render(<Input {...testProps} />);
+
+    const input = await screen.findByLabelText(testProps.label);
+    expect(input).toHaveAttribute('type', 'text');
+  });
+
+  it('does not show required message after render', () => {
+    render(<Input {...testProps} />);
+
+    const validation = screen.queryByText(/required/i);
+    expect(validation).not.toBeInTheDocument();
+  });
+
+  it('displays required message correctly', async () => {
+    const validationError = { touched: { city: true }, errors: { city: 'Required' } };
+    render(<Input {...testProps} {...validationError} />);
+
+    const validationMessage = await screen.findByText(/required/i);
+    expect(validationMessage).toBeInTheDocument();
+  });
 });
